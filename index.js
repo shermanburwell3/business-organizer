6299c6b9cf725c1898ef7b251442efe47d5b7579
Sherman Burwell <shermanburwell3@gmail.com>
2024-08-01 15:10:15 -0500

    Added function calls to switch case statement

--- index.js
+++ index.js
@@ -22,28 +22,28 @@ function Start() {
             "Exit"
         ]
     }).then((answers) => {
-        console.log(answers.mainMenuSelection);
+        console.log(answers.selection);
         switch (answers.selection) {
             case "View Departments":
-
+                viewDepartments();
                 break;
             case "View Roles":
-
+                viewRoles();
                 break;
             case "View Employees":
-
+                viewEmployees();
                 break;
             case "Add a Department":
-
+                addDepartment();
                 break;
             case "Add a Role":
-
+                addRole();
                 break;
             case "Add an Employee":
-
+                addEmployee();
                 break;
             case "Update an Employee Record":
-
+                updateEmployee();
                 break;
             case "Exit":
                 Exit();
@@ -58,49 +58,49 @@ function Start() {
 // View departments function
 function viewDepartments() {
 
-
+    console.log("vd");
 
 }
 
 // View roles function
 function viewRoles() {
 
-
+    console.log("vr");
 
 }
 
 // View employees function
 function viewEmployees() {
 
-
+    console.log("ve");
 
 }
 
 //Add department function
 function addDepartment() {
 
-
+    console.log("ad");
 
 }
 
 // Add role function
 function addRole() {
 
-
+    console.log("ar");
 
 }
 
 // Add employee function
 function addEmployee() {
 
-
+    console.log("ae");
 
 }
 
 // Update employee function
 function updateEmployee() {
 
-
+    console.log("ue");
 
 }
 
