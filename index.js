4e99e7a1eba2cf43a76b0668f4a21f50cec7c7c5
Sherman Burwell <shermanburwell3@gmail.com>
2024-08-01 14:05:08 -0500

    Wrote TODO comments to outline my functions

---
+++ index.js
@@ -0,0 +1,32 @@
+const inq = require('inquirer');
+const {Pool} = require('pg');
+
+//TODO: Add start funtion, bringing user to main menu
+
+
+
+//TODO: Add view departments function
+
+
+
+//TODO: Add view roles function
+
+
+
+//TODO: Add view employees function
+
+
+
+//TODO: Add add department function
+
+
+
+//TODO: Add add role function
+
+
+
+//TODO: Add add employee function
+
+
+
+//TODO: Add update employee function
