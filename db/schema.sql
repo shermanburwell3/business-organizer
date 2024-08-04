b96da512f446885dfd7d2ddcaf5590ee6c2d6087
Sherman Burwell <shermanburwell3@gmail.com>
2024-08-03 21:27:59 -0500

    Added role table into schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,12 @@ CREATE TABLE department (
     name VARCHAR(30) UNIQUE NOT NULL,
 );
 
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
